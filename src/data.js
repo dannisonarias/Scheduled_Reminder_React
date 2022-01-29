1261f12ffb63b4fbd7b1cef1d4815dbe6269120e
Dannison Arias <dannisonarias@gmail.com>
2022-01-29 09:25:21 -0500

    add scheduler

--- src/data.js
+++ src/data.js
@@ -1,37 +1,32 @@
 export default [
   {
     id: 1,
-    name: 'Bertie Yates',
-    age: 29,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    name: "clean the snow!",
+    time: 1,
+    image: "/images/shovelsnow.jpg",
   },
   {
     id: 2,
-    name: 'Hester Hogan',
-    age: 32,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    name: "Shower the doggy!",
+    time: 2,
+    image: "/images/doggy.jpg",
   },
   {
     id: 3,
-    name: 'Larry Little',
-    age: 36,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    name: "1 Mile Bike Ride",
+    time: 3,
+    image: "/images/bikeride.jpg",
   },
   {
     id: 4,
-    name: 'Sean Walsh',
-    age: 34,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    name: "Hair cut today!",
+    time: 4,
+    image: "/images/haircut.jpg",
   },
   {
     id: 5,
-    name: 'Lola Gardner',
-    age: 29,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    name: "Read a book",
+    time: 5,
+    image: "/images/readabook.jpg",
   },
 ];
